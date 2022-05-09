66a49321c30504b6c861b2bdd5190876bbbb482b
airacalins <airacalins@gmail.com>
2022-05-10 00:39:18 +0800

    added tab bar

--- lib/home.dart
+++ lib/home.dart
@@ -16,49 +16,72 @@ class _HomeState extends State<Home> {
 ;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        centerTitle: false,
-        leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image(
-            image: AssetImage('assets/images/logo.png'),
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            // ignore: prefer_const_literals_to_create_immutables
+            tabs: [
+              Tab(
+                text: 'Car Repair',
+                icon: Icon(Icons.car_repair),
+              ),
+              Tab(
+                text: 'Home Repair',
+                icon: Icon(Icons.home_repair_service),
+              ),
+              Tab(
+                text: 'Room Service',
+                icon: Icon(Icons.room_service),
+              ),
+            ],
           ),
-        ),
-        title: Text(
-          appBarTitle,
-        ),
-        actions: [
-          IconButton(
-            splashRadius: 30,
-            onPressed: () => setState(() => text = 'Casting clicked!'), 
-            icon: Icon(Icons.cast),
-          ),
-          IconButton(
-            splashRadius: 30,
-            onPressed: () => setState(() => text = 'Notifications clicked!'), 
-            icon: Icon(Icons.notifications),
-          ),
-          IconButton(
-            splashRadius: 30,
-            onPressed: () => setState(() => text = 'Search clicked!'), 
-            icon: Icon(Icons.search),
-          ),
-          IconButton(
-            splashRadius: 30,
-            onPressed: () => setState(() => drawerKey.currentState?.openEndDrawer()), 
-            icon: CircleAvatar(
-              backgroundImage: AssetImage('assets/images/profile.jpg'),
+          backgroundColor: Colors.blueGrey,
+          centerTitle: false,
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image(
+              image: AssetImage('assets/images/logo.png'),
             ),
           ),
-        ],
-      ),
-      endDrawer: Drawer(),
-      body: Center(
-        child: Text(
-          text
+          title: Text(
+            appBarTitle,
+          ),
+          actions: [
+            IconButton(
+              splashRadius: 30,
+              onPressed: () => setState(() => text = 'Casting clicked!'), 
+              icon: Icon(Icons.cast),
+            ),
+            IconButton(
+              splashRadius: 30,
+              onPressed: () => setState(() => text = 'Notifications clicked!'), 
+              icon: Icon(Icons.notifications),
+            ),
+            IconButton(
+              splashRadius: 30,
+              onPressed: () => setState(() => text = 'Search clicked!'), 
+              icon: Icon(Icons.search),
+            ),
+            IconButton(
+              splashRadius: 30,
+              onPressed: () => setState(() => drawerKey.currentState?.openEndDrawer()), 
+              icon: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/profile.jpg'),
+              ),
+            ),
+          ],
         ),
+        endDrawer: Drawer(),
+        body: TabBarView(
+          children: [
+            Center(child: Text('Car Repair - $text',),),
+            Center(child: Text('Home Repair - $text',),),
+            Center(child: Text('Room Service - $text',),),
+          ],
+        )
       ),
     );
   }
